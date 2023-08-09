a730e5712b9598a53fdb74f9661c7d6cdc676c22
Vavavoom <42918656+Thaleia@users.noreply.github.com>
2023-08-09 13:28:45 +0100

    website target change

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CharityComponent from './components/CharityComponent'
+
+function App() {
+  return (
+    <div className='App'>
+      <CharityComponent />
+    </div>
+  )
+}
+
+export default App;
